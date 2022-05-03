094287230f772fb1feea5c2f321beb11209fcb4e
Jelon <jelongames@gmail.com>
2022-05-03 10:25:13 +0200

    Conditions in react

--- react-app-master/src/components/counter.jsx
+++ react-app-master/src/components/counter.jsx
@@ -10,19 +10,29 @@ class Counter extends Component {
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        {this.renderTags()}
       </React.Fragment>
     );
   }
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
   getBadgeClasses() {
     let classes = "badge m-2 bg-";
     classes += this.state.count === 0 ? "warning" : "primary";
     return classes;
   }
+
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
